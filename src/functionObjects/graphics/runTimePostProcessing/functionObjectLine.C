demarle/openfoamvismodule

ENH: improve file reader support for runTimePostProcessing (#1091)
- support .vtp format for geometry, surface, line, cloud.

- use native reader for handling vtk, vtp, obj, stl surface files.
  For other formats, use the MeshedSurface (the surfMesh lib) to
  handle reading and Foam::vtk::Tools::Patch to handle the
  conversion to vtkPolyData. This combination is more memory efficient.

- update tutorial case to include vtp surface geometry

src/runTimePostProcessing/functionObjectLine.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2015-2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2015-2018 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -30,11 +30,15 @@ License
 
 // VTK includes
 #include "vtkActor.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
 #include "vtkRenderer.h"
 #include "vtkSmartPointer.h"
-#include "vtkPolyDataMapper.h"
+
+// VTK Readers
 #include "vtkPolyDataReader.h"
-#include "vtkProperty.h"
+#include "vtkXMLPolyDataReader.h"
 
 // * * * * * * * * * * * * * Static Member Functions * * * * * * * * * * * * //
 
@@ -51,13 +55,51 @@ namespace runTimePostPro
 }
 
 
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+namespace
+{
+
+static vtkSmartPointer<vtkPolyData> getPolyDataFile(const Foam::fileName& fName)
+{
+    // Not extremely elegant...
+    vtkSmartPointer<vtkPolyData> dataset;
+
+    if (fName.ext() == "vtk")
+    {
+        auto reader = vtkSmartPointer<vtkPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "vtp")
+    {
+        auto reader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    return dataset;
+}
+
+} // End anonymous namespace
+
+
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 Foam::functionObjects::runTimePostPro::functionObjectLine::functionObjectLine
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     pathline(parent, dict, colours),
@@ -99,21 +141,28 @@ addGeometryToScene
         return;
     }
 
-    if (fName.hasExt("vtk"))
+
+    auto polyData = getPolyDataFile(fName);
+
+    if (!polyData || polyData->GetNumberOfPoints() == 0)
     {
-        auto lines = vtkSmartPointer<vtkPolyDataReader>::New();
-        lines->SetFileName(fName.c_str());
-        lines->Update();
-
-        auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-        setField(position, fieldName_, mapper, renderer, lines->GetOutput());
-
-        actor_->SetMapper(mapper);
-
-        addLines(position, actor_, lines->GetOutput());
-
-        renderer->AddActor(actor_);
+        WarningInFunction
+            << "Could not read "<< fName << nl
+            << "Only VTK (.vtp, .vtk) files are supported"
+            << endl;
+        return;
     }
+
+
+    auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+
+    setField(position, fieldName_, mapper, renderer, polyData);
+
+    actor_->SetMapper(mapper);
+
+    addLines(position, actor_, polyData);
+
+    renderer->AddActor(actor_);
 }
 
 
@@ -125,6 +174,7 @@ void Foam::functionObjects::runTimePostPro::functionObjectLine::updateActors
     actor_->GetProperty()->SetLineWidth(2);
     actor_->GetProperty()->SetOpacity(opacity(position));
 }
+
 
 bool Foam::functionObjects::runTimePostPro::functionObjectLine::clear()
 {

src/runTimePostProcessing/geometrySurface.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2015-2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2015-2018 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -26,7 +26,8 @@ License
 // OpenFOAM includes
 #include "geometrySurface.H"
 #include "stringOps.H"
-#include "triSurface.H"
+#include "foamVtkTools.H"
+#include "MeshedSurfaces.H"
 #include "runTimePostProcessing.H"
 #include "addToRunTimeSelectionTable.H"
 
@@ -42,7 +43,12 @@ License
 #include "vtkProperty.h"
 #include "vtkRenderer.h"
 #include "vtkSmartPointer.h"
-#include "vtkTriangle.h"
+
+// VTK Readers
+#include "vtkOBJReader.h"
+#include "vtkSTLReader.h"
+#include "vtkPolyDataReader.h"
+#include "vtkXMLPolyDataReader.h"
 
 // * * * * * * * * * * * * * Static Member Functions * * * * * * * * * * * * //
 
@@ -57,6 +63,78 @@ namespace runTimePostPro
 }
 }
 }
+
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+namespace
+{
+
+static vtkSmartPointer<vtkPolyData> getPolyDataFile(const Foam::fileName& fName)
+{
+    // Not extremely elegant...
+    vtkSmartPointer<vtkPolyData> dataset;
+
+    if (fName.ext() == "vtk")
+    {
+        auto reader = vtkSmartPointer<vtkPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "vtp")
+    {
+        auto reader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "obj")
+    {
+        auto reader = vtkSmartPointer<vtkOBJReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "stl" || fName.ext() == "stlb")
+    {
+        auto reader = vtkSmartPointer<vtkSTLReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+
+    // Fallback to using OpenFOAM to read the surface and convert afterwards
+    Foam::meshedSurface surf(fName);
+
+    dataset = Foam::vtk::Tools::Patch::mesh(surf);
+
+    dataset->GetCellData()->SetNormals
+    (
+        Foam::vtk::Tools::Patch::faceNormals(surf)
+    );
+
+    return dataset;
+}
+
+} // End anonymous namespace
+
 
 // * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
 
@@ -74,53 +152,22 @@ void Foam::functionObjects::runTimePostPro::geometrySurface::addGeometryToScene
             << " object" << exit(FatalError);
     }
 
-    triSurface surf(fName);
+    auto surf = getPolyDataFile(fName);
 
-    const Field<point>& surfPoints = surf.points();
-    const Field<vector>& surfFaceNormals = surf.faceNormals();
-
-    auto points = vtkSmartPointer<vtkPoints>::New();
-    auto triangles = vtkSmartPointer<vtkCellArray>::New();
-    auto faceNormals = vtkSmartPointer<vtkDoubleArray>::New();
-
-    faceNormals->SetNumberOfComponents(3);
-
-    forAll(surfPoints, i)
+    if (!surf || surf->GetNumberOfPoints() == 0)
     {
-        const point& pt = surfPoints[i];
-        points->InsertNextPoint(pt.x(), pt.y(), pt.z());
+        FatalErrorInFunction
+            << "Could not read "<< fName << nl
+            << exit(FatalError);
     }
-
-    forAll(surf, i)
-    {
-        const Foam::face& f = surf[i];
-
-        auto triangle = vtkSmartPointer<vtkTriangle>::New();
-        triangle->GetPointIds()->SetId(0, f[0]);
-        triangle->GetPointIds()->SetId(1, f[1]);
-        triangle->GetPointIds()->SetId(2, f[2]);
-        triangles->InsertNextCell(triangle);
-
-        double n[3];
-        n[0] = surfFaceNormals[i].x();
-        n[1] = surfFaceNormals[i].y();
-        n[2] = surfFaceNormals[i].z();
-
-        faceNormals->InsertNextTuple(n);
-    }
-
-    surf.clearOut();
-
-    auto polyData = vtkSmartPointer<vtkPolyData>::New();
-    polyData->SetPoints(points);
-    polyData->SetPolys(triangles);
-    polyData->GetCellData()->SetNormals(faceNormals);
 
     auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->ScalarVisibilityOff();
-    mapper->SetInputData(polyData);
 
-    addFeatureEdges(renderer, polyData);
+    mapper->ScalarVisibilityOff();
+
+    mapper->SetInputData(surf);
+
+    addFeatureEdges(renderer, surf);
 
     surfaceActor_->SetMapper(mapper);
 
@@ -136,7 +183,7 @@ Foam::functionObjects::runTimePostPro::geometrySurface::geometrySurface
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     surface(parent, dict, colours),
@@ -150,18 +197,12 @@ Foam::functionObjects::runTimePostPro::geometrySurface::geometrySurface
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours,
+    const HashPtrTable<Function1<vector>>& colours,
     const List<fileName>& fileNames
 )
 :
     surface(parent, dict, colours),
     fileNames_(fileNames)
-{}
-
-
-// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
-
-Foam::functionObjects::runTimePostPro::geometrySurface::~geometrySurface()
 {}
 
 
@@ -178,10 +219,10 @@ void Foam::functionObjects::runTimePostPro::geometrySurface::addGeometryToScene
         return;
     }
 
-    forAll(fileNames_, i)
+    for (fileName f : fileNames_)  // Use a copy
     {
-        fileName fName = fileNames_[i].expand();
-        addGeometryToScene(position, renderer, fName);
+        f.expand();
+        addGeometryToScene(position, renderer, f);
     }
 }
 
@@ -210,8 +251,8 @@ void Foam::functionObjects::runTimePostPro::geometrySurface::updateActors
 
 bool Foam::functionObjects::runTimePostPro::geometrySurface::clear()
 {
-    // Note: not removing geometry files
-    // - these are usually static files that are used e.g. for meshing
+    // Note: do not remove geometry files
+    // - often static files used for other purposes as well (eg meshing)
 
     return true;
 }

src/runTimePostProcessing/pathline.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2015 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2015-2018 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -78,7 +78,7 @@ void Foam::functionObjects::runTimePostPro::pathline::addLines
     actor->GetProperty()->SetColor(colour[0], colour[1], colour[2]);
 
     vtkPolyDataMapper* mapper =
-            vtkPolyDataMapper::SafeDownCast(actor->GetMapper());
+        vtkPolyDataMapper::SafeDownCast(actor->GetMapper());
 
     switch (representation_)
     {
@@ -123,7 +123,7 @@ Foam::functionObjects::runTimePostPro::pathline::pathline
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     geometryBase(parent, dict, colours),
@@ -136,7 +136,7 @@ Foam::functionObjects::runTimePostPro::pathline::pathline
 {
     if (dict.found("lineColour"))
     {
-        lineColour_.reset(Function1<vector>::New("lineColour", dict).ptr());
+        lineColour_.reset(Function1<vector>::New("lineColour", dict));
     }
     else
     {
@@ -174,7 +174,7 @@ Foam::functionObjects::runTimePostPro::pathline::New
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours,
+    const HashPtrTable<Function1<vector>>& colours,
     const word& pathlineType
 )
 {

src/runTimePostProcessing/pointData.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2015-2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2015-2018 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -93,7 +93,7 @@ Foam::functionObjects::runTimePostPro::pointData::pointData
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     geometryBase(parent, dict, colours),
@@ -106,7 +106,7 @@ Foam::functionObjects::runTimePostPro::pointData::pointData
 {
     if (dict.found("pointColour"))
     {
-        pointColour_.reset(Function1<vector>::New("pointColour", dict).ptr());
+        pointColour_.reset(Function1<vector>::New("pointColour", dict));
     }
     else
     {
@@ -135,7 +135,7 @@ Foam::functionObjects::runTimePostPro::pointData::New
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours,
+    const HashPtrTable<Function1<vector>>& colours,
     const word& pointDataType
 )
 {

src/runTimePostProcessing/runTimePostProcessingTemplates.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2015 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2015-2018 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License

RemoteConnectionManager/openfoam-dev

src/functionObjects/graphics/runTimePostProcessing/functionObjectLine.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015-2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2015-2018 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -30,11 +30,15 @@ License
 
 // VTK includes
 #include "vtkActor.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
 #include "vtkRenderer.h"
 #include "vtkSmartPointer.h"
-#include "vtkPolyDataMapper.h"
+
+// VTK Readers
 #include "vtkPolyDataReader.h"
-#include "vtkProperty.h"
+#include "vtkXMLPolyDataReader.h"
 
 // * * * * * * * * * * * * * Static Member Functions * * * * * * * * * * * * //
 
@@ -51,13 +55,51 @@ namespace runTimePostPro
 }
 
 
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+namespace
+{
+
+static vtkSmartPointer<vtkPolyData> getPolyDataFile(const Foam::fileName& fName)
+{
+    // Not extremely elegant...
+    vtkSmartPointer<vtkPolyData> dataset;
+
+    if (fName.ext() == "vtk")
+    {
+        auto reader = vtkSmartPointer<vtkPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "vtp")
+    {
+        auto reader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    return dataset;
+}
+
+} // End anonymous namespace
+
+
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 Foam::functionObjects::runTimePostPro::functionObjectLine::functionObjectLine
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     pathline(parent, dict, colours),
@@ -99,21 +141,28 @@ addGeometryToScene
         return;
     }
 
-    if (fName.hasExt("vtk"))
+
+    auto polyData = getPolyDataFile(fName);
+
+    if (!polyData || polyData->GetNumberOfPoints() == 0)
     {
-        auto lines = vtkSmartPointer<vtkPolyDataReader>::New();
-        lines->SetFileName(fName.c_str());
-        lines->Update();
-
-        auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-        setField(position, fieldName_, mapper, renderer, lines->GetOutput());
-
-        actor_->SetMapper(mapper);
-
-        addLines(position, actor_, lines->GetOutput());
-
-        renderer->AddActor(actor_);
+        WarningInFunction
+            << "Could not read "<< fName << nl
+            << "Only VTK (.vtp, .vtk) files are supported"
+            << endl;
+        return;
     }
+
+
+    auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+
+    setField(position, fieldName_, mapper, renderer, polyData);
+
+    actor_->SetMapper(mapper);
+
+    addLines(position, actor_, polyData);
+
+    renderer->AddActor(actor_);
 }
 
 
@@ -125,6 +174,7 @@ void Foam::functionObjects::runTimePostPro::functionObjectLine::updateActors
     actor_->GetProperty()->SetLineWidth(2);
     actor_->GetProperty()->SetOpacity(opacity(position));
 }
+
 
 bool Foam::functionObjects::runTimePostPro::functionObjectLine::clear()
 {

src/functionObjects/graphics/runTimePostProcessing/geometrySurface.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015-2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2015-2018 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -26,7 +26,8 @@ License
 // OpenFOAM includes
 #include "geometrySurface.H"
 #include "stringOps.H"
-#include "triSurface.H"
+#include "foamVtkTools.H"
+#include "MeshedSurfaces.H"
 #include "runTimePostProcessing.H"
 #include "addToRunTimeSelectionTable.H"
 
@@ -42,7 +43,12 @@ License
 #include "vtkProperty.h"
 #include "vtkRenderer.h"
 #include "vtkSmartPointer.h"
-#include "vtkTriangle.h"
+
+// VTK Readers
+#include "vtkOBJReader.h"
+#include "vtkSTLReader.h"
+#include "vtkPolyDataReader.h"
+#include "vtkXMLPolyDataReader.h"
 
 // * * * * * * * * * * * * * Static Member Functions * * * * * * * * * * * * //
 
@@ -57,6 +63,78 @@ namespace runTimePostPro
 }
 }
 }
+
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+namespace
+{
+
+static vtkSmartPointer<vtkPolyData> getPolyDataFile(const Foam::fileName& fName)
+{
+    // Not extremely elegant...
+    vtkSmartPointer<vtkPolyData> dataset;
+
+    if (fName.ext() == "vtk")
+    {
+        auto reader = vtkSmartPointer<vtkPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "vtp")
+    {
+        auto reader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "obj")
+    {
+        auto reader = vtkSmartPointer<vtkOBJReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+    if (fName.ext() == "stl" || fName.ext() == "stlb")
+    {
+        auto reader = vtkSmartPointer<vtkSTLReader>::New();
+
+        reader->SetFileName(fName.c_str());
+        reader->Update();
+        dataset = reader->GetOutput();
+
+        return dataset;
+    }
+
+
+    // Fallback to using OpenFOAM to read the surface and convert afterwards
+    Foam::meshedSurface surf(fName);
+
+    dataset = Foam::vtk::Tools::Patch::mesh(surf);
+
+    dataset->GetCellData()->SetNormals
+    (
+        Foam::vtk::Tools::Patch::faceNormals(surf)
+    );
+
+    return dataset;
+}
+
+} // End anonymous namespace
+
 
 // * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
 
@@ -74,53 +152,22 @@ void Foam::functionObjects::runTimePostPro::geometrySurface::addGeometryToScene
             << " object" << exit(FatalError);
     }
 
-    triSurface surf(fName);
+    auto surf = getPolyDataFile(fName);
 
-    const Field<point>& surfPoints = surf.points();
-    const Field<vector>& surfFaceNormals = surf.faceNormals();
-
-    auto points = vtkSmartPointer<vtkPoints>::New();
-    auto triangles = vtkSmartPointer<vtkCellArray>::New();
-    auto faceNormals = vtkSmartPointer<vtkDoubleArray>::New();
-
-    faceNormals->SetNumberOfComponents(3);
-
-    forAll(surfPoints, i)
+    if (!surf || surf->GetNumberOfPoints() == 0)
     {
-        const point& pt = surfPoints[i];
-        points->InsertNextPoint(pt.x(), pt.y(), pt.z());
+        FatalErrorInFunction
+            << "Could not read "<< fName << nl
+            << exit(FatalError);
     }
-
-    forAll(surf, i)
-    {
-        const Foam::face& f = surf[i];
-
-        auto triangle = vtkSmartPointer<vtkTriangle>::New();
-        triangle->GetPointIds()->SetId(0, f[0]);
-        triangle->GetPointIds()->SetId(1, f[1]);
-        triangle->GetPointIds()->SetId(2, f[2]);
-        triangles->InsertNextCell(triangle);
-
-        double n[3];
-        n[0] = surfFaceNormals[i].x();
-        n[1] = surfFaceNormals[i].y();
-        n[2] = surfFaceNormals[i].z();
-
-        faceNormals->InsertNextTuple(n);
-    }
-
-    surf.clearOut();
-
-    auto polyData = vtkSmartPointer<vtkPolyData>::New();
-    polyData->SetPoints(points);
-    polyData->SetPolys(triangles);
-    polyData->GetCellData()->SetNormals(faceNormals);
 
     auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->ScalarVisibilityOff();
-    mapper->SetInputData(polyData);
 
-    addFeatureEdges(renderer, polyData);
+    mapper->ScalarVisibilityOff();
+
+    mapper->SetInputData(surf);
+
+    addFeatureEdges(renderer, surf);
 
     surfaceActor_->SetMapper(mapper);
 
@@ -136,7 +183,7 @@ Foam::functionObjects::runTimePostPro::geometrySurface::geometrySurface
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     surface(parent, dict, colours),
@@ -150,18 +197,12 @@ Foam::functionObjects::runTimePostPro::geometrySurface::geometrySurface
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours,
+    const HashPtrTable<Function1<vector>>& colours,
     const List<fileName>& fileNames
 )
 :
     surface(parent, dict, colours),
     fileNames_(fileNames)
-{}
-
-
-// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
-
-Foam::functionObjects::runTimePostPro::geometrySurface::~geometrySurface()
 {}
 
 
@@ -178,10 +219,10 @@ void Foam::functionObjects::runTimePostPro::geometrySurface::addGeometryToScene
         return;
     }
 
-    forAll(fileNames_, i)
+    for (fileName f : fileNames_)  // Use a copy
     {
-        fileName fName = fileNames_[i].expand();
-        addGeometryToScene(position, renderer, fName);
+        f.expand();
+        addGeometryToScene(position, renderer, f);
     }
 }
 
@@ -210,8 +251,8 @@ void Foam::functionObjects::runTimePostPro::geometrySurface::updateActors
 
 bool Foam::functionObjects::runTimePostPro::geometrySurface::clear()
 {
-    // Note: not removing geometry files
-    // - these are usually static files that are used e.g. for meshing
+    // Note: do not remove geometry files
+    // - often static files used for other purposes as well (eg meshing)
 
     return true;
 }

src/functionObjects/graphics/runTimePostProcessing/pathline.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2015-2018 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -78,7 +78,7 @@ void Foam::functionObjects::runTimePostPro::pathline::addLines
     actor->GetProperty()->SetColor(colour[0], colour[1], colour[2]);
 
     vtkPolyDataMapper* mapper =
-            vtkPolyDataMapper::SafeDownCast(actor->GetMapper());
+        vtkPolyDataMapper::SafeDownCast(actor->GetMapper());
 
     switch (representation_)
     {
@@ -123,7 +123,7 @@ Foam::functionObjects::runTimePostPro::pathline::pathline
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     geometryBase(parent, dict, colours),
@@ -136,7 +136,7 @@ Foam::functionObjects::runTimePostPro::pathline::pathline
 {
     if (dict.found("lineColour"))
     {
-        lineColour_.reset(Function1<vector>::New("lineColour", dict).ptr());
+        lineColour_.reset(Function1<vector>::New("lineColour", dict));
     }
     else
     {
@@ -174,7 +174,7 @@ Foam::functionObjects::runTimePostPro::pathline::New
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours,
+    const HashPtrTable<Function1<vector>>& colours,
     const word& pathlineType
 )
 {

src/functionObjects/graphics/runTimePostProcessing/pointData.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015-2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2015-2018 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -93,7 +93,7 @@ Foam::functionObjects::runTimePostPro::pointData::pointData
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours
+    const HashPtrTable<Function1<vector>>& colours
 )
 :
     geometryBase(parent, dict, colours),
@@ -106,7 +106,7 @@ Foam::functionObjects::runTimePostPro::pointData::pointData
 {
     if (dict.found("pointColour"))
     {
-        pointColour_.reset(Function1<vector>::New("pointColour", dict).ptr());
+        pointColour_.reset(Function1<vector>::New("pointColour", dict));
     }
     else
     {
@@ -135,7 +135,7 @@ Foam::functionObjects::runTimePostPro::pointData::New
 (
     const runTimePostProcessing& parent,
     const dictionary& dict,
-    const HashPtrTable<Function1<vector>, word>& colours,
+    const HashPtrTable<Function1<vector>>& colours,
     const word& pointDataType
 )
 {

src/functionObjects/graphics/runTimePostProcessing/runTimePostProcessingTemplates.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2015-2018 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.